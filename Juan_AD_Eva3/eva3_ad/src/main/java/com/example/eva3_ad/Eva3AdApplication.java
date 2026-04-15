e40e328cfb803961f6706143d8db6e649c78f282
22482772-ship-it <22482772@live.uem.es>
2026-04-15 18:01:05 +0200

    eva3_ad

---
+++ Juan_AD_Eva3/eva3_ad/src/main/java/com/example/eva3_ad/Eva3AdApplication.java
@@ -0,0 +1,13 @@
+package com.example.eva3_ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eva3AdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Eva3AdApplication.class, args);
+	}
+
+}
---
+++ Juan_AD_Eva3/eva3_ad/src/test/java/com/example/eva3_ad/Eva3AdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eva3_ad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eva3AdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
